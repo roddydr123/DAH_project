a3e5d046cce9669a3fe83f067a97c7e9532dce07
David Roddy <davidroddy321@gmail.com>
2022-11-17 16:05:40 +0000

    method for fahrenheit

--- scrape.py
+++ scrape.py
@@ -21,6 +21,9 @@ class Measurement():
                 F = self.get('temp')
                 return self.convert(F)
 
+        def get_temp_F(self):
+                return self.get('temp')
+
 
 if __name__=="__main__":
         meas = Measurement()
