22e5c92f3d9bb9e370ce20b9eb7ea087e690f96f
David Roddy <davidroddy321@gmail.com>
2022-11-21 15:51:10 +0000

    plot temperature over time

--- animated.py
+++ animated.py
@@ -1,26 +1,30 @@
 from scrape import Measurement
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import datetime
 
 
-def updatePlot(i, meas, plotFigure, temps, hums):
+def updatePlot(i, meas, plotFigure, temps, times, start):
+        now = datetime.datetime.now()
         temps.append(meas.get_temp())
-        hums.append(meas.get_humidity())
+        diff = now - start
+        times.append(diff.total_seconds())
 
         plotFigure.clear()
 
-        plt.scatter(hums, temps)
-        plt.title("Temperature vs Humidity")
-        plt.xlabel("Humidity (%)")
+        plt.scatter(times, temps)
+        plt.title("Temperature vs time")
+        plt.xlabel("Time after start (s)")
         plt.ylabel("Temperature (degrees Celsius)")
 
 
 def main():
+        start = datetime.datetime.now()
         plotFigure = plt.figure()
         meas = Measurement()
         temps = []
-        hums = []
-        ani = FuncAnimation(plotFigure, updatePlot, interval=10, fargs=(meas, plotFigure, temps, hums))
+        times = []
+        ani = FuncAnimation(plotFigure, updatePlot, interval=10, fargs=(meas, plotFigure, temps, times, start))
         plt.show()
 
 
